d5f40a6bb44bc85634d8a89e4ee060fd8c108ea4
wangqian <925548289@qq.com>
2021-01-22 15:00:24 +0800

    - 初始化网关配置

---
+++ src/main/java/com/aster/cloud/svc/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.aster.cloud.svc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ *
+ * @author 王骞
+ * @date 2021-01-22
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
